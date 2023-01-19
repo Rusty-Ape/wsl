0dcaa648b2dfa23a69eace813b55076e3ded8824
Aise <goodluck.arasomwan@gmail.com>
2023-01-19 22:02:55 +0100

    Testing

---
+++ _next/static/qLNBy2Y8DzZ2aGWwE6rwD/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/9775fe597d0c1f4a.css","static/chunks/pages/index-de159072e33ff354.js"],"/_error":["static/chunks/pages/_error-409f831d3504c8f5.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
